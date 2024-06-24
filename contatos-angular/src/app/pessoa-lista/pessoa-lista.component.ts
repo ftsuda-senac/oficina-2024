d79635e8425a57e1683ac5fe1ef09fe0226cf9a8
ftsuda-senac <ftsuda.senac@gmail.com>
2024-06-24 19:31:55 -0300

    feat: Funcionalidades de exclusão

--- contatos-angular/src/app/pessoa-lista/pessoa-lista.component.ts
+++ contatos-angular/src/app/pessoa-lista/pessoa-lista.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterModule } from '@angular/router';
 import { PessoaService } from '../pessoa.service';
 import { Pessoa } from '../pessoa';
 import { PessoaRestService } from '../pessoa-rest.service';
 import { Observable } from 'rxjs';
-import { ModalDismissReasons, NgbDatepickerModule, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 type Alerta = {
   mensagem: string,
@@ -37,11 +37,13 @@ export class PessoaListaComponent {
 
   alerta?: Alerta | null;
 
-  modal?: ModalInfo | null;
+  modalInfo?: ModalInfo | null;
+
+  private modalService = inject(NgbModal);
 
   constructor(private pessoaService: PessoaRestService, router: Router) {
     this.listaPessoas$ = this.pessoaService.findAllPessoas(); // $ no final é convençao do angular para indicar que é Observable
-    this.alerta =  router.getCurrentNavigation()?.extras.state as Alerta
+    this.alerta = router.getCurrentNavigation()?.extras.state as Alerta
   }
 
   convertIsoDateToDate(isoDate?: string): Date | undefined {
@@ -62,16 +64,27 @@ export class PessoaListaComponent {
     }
   }
 
-  confirmarExclusao(id: number): void {
-    console.log('exclusao: ' + id)
-    this.modal = {
+  abrirModalExclusao(id: number, modalDelete: any): void {
+    this.modalInfo = {
       mensagem: 'Confirma exclusão da pessoa ID ' + id + '?',
       id: id
     }
+    this.modalService.open(modalDelete, { centered: true });
+  }
+
+  excluir(id: number) {
+    this.pessoaService.remove(id).subscribe(() => {
+      this.listaPessoas$ = this.pessoaService.findAllPessoas();
+      this.modalService.dismissAll();
+      this.alerta = {
+        mensagem: 'Pessoa excluída com sucesso',
+        tipo: 'sucesso'
+      }
+    });
   }
 
   fecharModal(): void {
-    this.modal = null;
+    this.modalInfo = null;
   }
 
 }
